b7249beaf8d0e61b8101eed7a4b37bc0f5218d5a
Hieu Phan <aznrice2k4@gmail.com>
2014-03-03 19:03:59 -0600

    Revert "mm: cma: WARN if freed memory is still in use"
    This reverts commit 7671cce2db99da104d87e216ba16c467083760d1.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6156,15 +6156,8 @@ done:
 
 void free_contig_range(unsigned long pfn, unsigned nr_pages)
 {
-	unsigned int count = 0;
-
-	for (; nr_pages--; pfn++) {
-		struct page *page = pfn_to_page(pfn);
-
-		count += page_count(page) != 1;
-		__free_page(page);
-	}
-	WARN(count != 0, "%d pages are still in use!\n", count);
+	for (; nr_pages--; ++pfn)
+		__free_page(pfn_to_page(pfn));
 }
 #endif
 
