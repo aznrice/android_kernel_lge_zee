64edc4b319e8b65b20ac6c88ec25d2f2dc735b39
Hieu Phan <aznrice2k4@gmail.com>
2014-03-04 23:21:40 -0600

    drivers/crypto/msm/qcedev.c: fix warning: array subscript is above array bounds [-Warray-bounds]

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -946,6 +946,7 @@ static int qcedev_sha_update(struct qcedev_async_req *qcedev_areq,
 				}
 
 				sreq->data_len = total;
+				if (i < 0) {
 				if (i > 0)
 					for (k = 0; k < num_entries; k++) {
 						sreq->data[k].len =
@@ -953,6 +954,7 @@ static int qcedev_sha_update(struct qcedev_async_req *qcedev_areq,
 						sreq->data[k].vaddr =
 							sreq->data[i+k].vaddr;
 					}
+				}
 				sreq->entries = num_entries;
 
 				i = j;
@@ -1536,6 +1538,7 @@ static int qcedev_vbuf_ablk_cipher(struct qcedev_async_req *areq,
 				}
 
 				creq->data_len = total;
+				if (i < 0) {
 				if (i > 0)
 					for (k = 0; k < num_entries; k++) {
 						creq->vbuf.src[k].len =
@@ -1543,6 +1546,7 @@ static int qcedev_vbuf_ablk_cipher(struct qcedev_async_req *areq,
 						creq->vbuf.src[k].vaddr =
 						creq->vbuf.src[i+k].vaddr;
 					}
+				}
 				creq->entries =  num_entries;
 
 				i = j;
