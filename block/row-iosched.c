5f73642a842842fe89483785a9704e6cec6cad27
Hieu Phan <aznrice2k4@gmail.com>
2014-03-20 02:23:24 -0500

    block/row-iosched.c: change to upstream caf 3.10 version
    Signed-off-by: Hieu Phan <aznrice2k4@gmail.com>

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -788,7 +788,7 @@ done:
  * this dispatch queue
  *
  */
-static void *row_init_queue(struct request_queue *q)
+static int row_init_queue(struct request_queue *q)
 {
 
 	struct row_data *rdata;
@@ -797,7 +797,7 @@ static void *row_init_queue(struct request_queue *q)
 	rdata = kmalloc_node(sizeof(*rdata),
 			     GFP_KERNEL | __GFP_ZERO, q->node);
 	if (!rdata)
-		return NULL;
+		return -ENOMEM;
 
 	memset(rdata, 0, sizeof(*rdata));
 	for (i = 0; i < ROWQ_MAX_PRIO; i++) {
@@ -829,8 +829,9 @@ static void *row_init_queue(struct request_queue *q)
 	rdata->last_served_ioprio_class = IOPRIO_CLASS_NONE;
 	rdata->rd_idle_data.idling_queue_idx = ROWQ_MAX_PRIO;
 	rdata->dispatch_queue = q;
+	q->elevator->elevator_data = rdata;
 
-	return rdata;
+	return 0;
 }
 
 /*
@@ -936,7 +937,8 @@ static enum row_queue_prio row_get_queue_prio(struct request *rq,
  *
  */
 static int
-row_set_request(struct request_queue *q, struct request *rq, gfp_t gfp_mask)
+row_set_request(struct request_queue *q, struct request *rq, struct bio *bio,
+		gfp_t gfp_mask)
 {
 	struct row_data *rd = (struct row_data *)q->elevator->elevator_data;
 	unsigned long flags;
